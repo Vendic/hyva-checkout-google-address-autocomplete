a981daefff80135f2765ad981a790775df761036
Frederik Rommel <frederik.rommel@webidea24.de>
2024-10-16 13:21:37 +0200

    fix: remove validation if not house-numnber-field is used

--- ViewModel/FieldMapping.php
+++ ViewModel/FieldMapping.php
@@ -17,4 +17,18 @@ class FieldMapping implements ArgumentInterface
     {
         return array_filter($this->fieldMapping);
     }
+
+    /**
+     * returns the index of the house-number field, if it is enabled
+     */
+    public function getHouseNumberFieldIndex(): ?int
+    {
+        preg_match('/^street\.(\d+)$/', $this->get()['street_number'] ?? '', $matches);
+        $houseNumberFieldIndex = $matches[1] ?? null;
+        if (is_numeric($houseNumberFieldIndex)) {
+            return (int) $houseNumberFieldIndex;
+        }
+
+        return null;
+    }
 }
